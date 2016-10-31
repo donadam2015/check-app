c0901bd71d34d688ae5ffdf150484ef2e5d1d7a2
Benjamin Foote <git@bnf.net>
2016-10-31 10:25:40 -0700

    add close method

--- testing/run_lots_of_tests.sh
+++ testing/run_lots_of_tests.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# change to where this script runs from 
+SDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
 
-NUMTEST=$1
-DOCKER_COMPOSE_FILE=docker-web-tester.yml
+DOCKER_COMPOSE_FILE=$SDIR/docker-web-tester.yml
 TESTCMD="web.test npm run test"
 # TESTCMD="web.test sleep 10"
+PROJECT=webtester
 
-if [ -z "${NUMTEST}" ]; then
+usage() {
     cat <<EOF
     usage:
         $0 numberoftests
+        $0 numberoftests close
 
     spins up additional compose groups of the check web tests and a chromium driver
+	or closes the testers
 
 EOF
     exit
-fi
+}
 
 dip () {
     CONT=$1
@@ -27,22 +31,35 @@ dip () {
 }
 
 new_instance() {
-    # use a different project name to differentiat the test instances
+    # use a different project name to differentiate the test instances
     local PROJECT=$1
-    docker-compose -f $DOCKER_COMPOSE_FILE -p $PROJECT down
     
-#    docker-compose -f $DOCKER_COMPOSE_FILE -p $PROJECT run -d $TESTCMD
+    docker-compose -f $DOCKER_COMPOSE_FILE -p $PROJECT run -d $TESTCMD
     echo "http://$(dip ${PROJECT}_web.test_run_1):13333"
     echo "vnc://$(dip ${PROJECT}_chromedriver_1)"
 }
 
+all_down() {
+    for I in $(seq 1 $NUMTEST); do
+    	docker-compose -f $DOCKER_COMPOSE_FILE -p webtester${I} down
+    done
+}
     
 main() {
 
     for I in $(seq 1 $NUMTEST); do
-	new_instance webtester${I};
+		new_instance ${PROJECT}${I};
     done
-
+	exit
 }
+
+NUMTEST=$1
+if [ -z "${NUMTEST}" ]; then
+	usage
+fi
+if [ "close" == "${2}" ]; then
+	all_down
+	exit
+fi
 
 main

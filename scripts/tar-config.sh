5dd43e62842edc74846cee9bd3bcfb95e0a75dc8
Benjamin Foote <git@bnf.net>
2016-10-31 10:03:52 -0700

    cleanup

--- scripts/tar-config.sh
+++ scripts/tar-config.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-filename=config-`date +%Y%m%d%H%M`.tar.gz
-tar zcf $filename check-api/config/config.yml check-api/config/database.yml pender/config/config.yml pender/config/database.yml check-web/config.js check-web/test/config.js check-web/test/config.yml
-echo Created file $filename
+
+FILENAME=config-$(date +%Y%m%d%H%M).tar.gz
+
+if [ -n "$1" ]; then
+	FILENAME=$1	
+fi
+
+tar zcf $FILENAME \
+	check-api/config/config.yml \
+	check-api/config/database.yml \
+	pender/config/config.yml \
+	pender/config/database.yml \
+	check-web/config.js \
+	check-web/test/config.js \
+	check-web/test/config.yml
+
+echo check config files: $FILENAME
